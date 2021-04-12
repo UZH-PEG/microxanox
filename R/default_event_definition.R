1294cd3a728b5b4a7f0785851ef76ce1e70ca529
Rainer M. Krug <Rainer@krugs.de>
2021-04-12 15:30:36 +0200

    Split files in R into one function per file

--- R/default_event_definition.R
+++ R/default_event_definition.R
@@ -1,4 +1,3 @@
-
 #' A function that contains the events that should alter the state variables
 #'
 #' @param times The time point in the simulation
@@ -25,60 +24,6 @@ default_event_definition <- function(times=times, y=state, parms = parameters) {
       N_SB <- minimum_abundances["SB"]
     
     return(c(N_CB, N_PB, N_SB, SO, SR, O, P))
-    }
-  )
-}
-
-
-#' A function that contains the events that should alter the state variables, with multiple strains
-#'
-#' @param times The time point in the simulation
-#' @param state Current state variable values
-#' @param parms Model parameters
-#' @return A vector of the state variables
-#' @export
-default_event_definition_strains <- function(times=times, y=state, parms = parameters) {
-  with(
-    as.list(y),
-    {
-      ## here adding a bit of noise to the abiotics
-      SO <- SO + rnorm(1, 0, noise_sigma*SO)*noise_sigma*SO
-      SR <- SR + rnorm(1, 0, noise_sigma*SR)*noise_sigma*SR
-      O <- O + rnorm(1, 0, noise_sigma*O)*noise_sigma*O
-      P <- P + rnorm(1, 0, noise_sigma*P)*noise_sigma*P
-      
-      
-      CB <- y[grep("CB", names(y))]
-      names_CB <- names(CB)[order(names(CB))]
-      CB <- as.numeric(CB[order(names(CB))])
-      CB[CB < minimum_abundances["CB"]] <- minimum_abundances["CB"]
-      
-      PB <- y[grep("PB", names(y))]
-      names_PB <- names(PB)[order(names(PB))]
-      PB <- as.numeric(PB[order(names(PB))])
-      PB[PB < minimum_abundances["PB"]] <- minimum_abundances["PB"]
-      
-      SB <- y[grep("SB", names(y))]
-      names_SB <- names(SB)[order(names(SB))]
-      SB <- as.numeric(SB[order(names(SB))])
-      SB[SB < minimum_abundances["SB"]] <- minimum_abundances["SB"]
-      
-      result <- c(CB,
-                  PB,
-                  SB,
-                  SO = SO,
-                  SR = SR,
-                  O = O,
-                  P = P)
-      
-      names(result) <- c(names_CB,
-                         names_PB,
-                         names_SB,
-                         "SO",
-                         "SR",
-                         "O",
-                         "P")
-      result
     }
   )
 }
