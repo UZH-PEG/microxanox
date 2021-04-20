859f96bc85bc2c2cd755b66e249cce1be435d443
Owen Petchey <owen.petchey@ieu.uzh.ch>
2021-04-20 17:25:43 +0200

    tiny thing

--- R/bushplus_dynamic_model_strains.R
+++ R/bushplus_dynamic_model_strains.R
@@ -1,6 +1,7 @@
 
 
-#' The rate equations, as published in the paper, but with forcing of oxygen diffusivity (a_0) potential added
+#' The rate equations, as published in the paper, but with forcing of oxygen diffusivity (a_0) potential added.
+#' And multiple strains
 #'
 #' @param t The current time in the simulation
 #' @param state A vector containing the current (named) values of each state variable
