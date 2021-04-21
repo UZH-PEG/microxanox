bf9a617c4013f9ae71c75582ea6668dd0ce42f78
Rainer M. Krug <Rainer@krugs.de>
2021-04-21 17:30:25 +0200

    Initialization of parameter

--- R/new_strain_parameter.R
+++ R/new_strain_parameter.R
@@ -1,25 +1,41 @@
 #' Creates empty parameter
 #' 
-#' If any of the arguments equals "bush", the default parameter from Bush et al. 2017 will be used for the general parameter
-#' @param n number of strains in the CB functional group or \code{"bush"}, in which case the default from Bush et al. 2017 will be used
-#' @param n number of strains in the PB functional group or \code{"bush"}, in which case the default from Bush et al. 2017 will be used
-#' @param n number of strains in the SB functional group or \code{"bush"}, in which case the default from Bush et al. 2017 will be used
+#' The default is to create the parameter set used in Bush et al. (2017)
+#' @param n_CB number of strains 
+#' @param values values_CB to be used for CB strains parameter or \code{"bush"}, in which case the default from Bush et al (2017) will be used
+#' @param n_PB number of strains 
+#' @param values values_PB to be used for PB strains parameter or \code{"bush"}, in which case the default from Bush et al (2017) will be used
+#' @param n_SB number of strains 
+#' @param values values_SB to be used for SB strains parameter or \code{"bush"}, in which case the default from Bush et al (2017) will be used
+#' @param values_other values to be used for other parameter or \code{"bush"}, in which case the default from Bush et al (2017) will be used
 #'
 #' @return \code{list} with additional class \code{strain_parameter}
 #' @export
 #'
 new_strain_parameter <- function(
   n_CB = 1,
+  values_CB = "bush",
   n_PB = 1,
-  n_SB = 1
+  values_PB = "bush",
+  n_SB = 1,
+  values_SB = "bush",
+  other = "bush"
 ){
+  if (is.na(other)) {
+    other <- "NA"
+  }
+  
+  if (!(other %in% c("NA", "bush"))) {
+    stop("Not supported value for `other`!\n", "Only NA, 'NA' and 'bush' supported!")
+  }
+  
   parms <- list()
 
 # strain parameter --------------------------------------------------------
 
-  parms$CB <- new_CB_strain_parameter(n = n_CB)
-  parms$PB <- new_PB_strain_parameter(n = n_PB)
-  parms$SB <- new_SB_strain_parameter(n = n_SB)
+  parms$CB <- new_CB_strain_parameter(n = n_CB, values = values_CB)
+  parms$PB <- new_PB_strain_parameter(n = n_PB, values = values_PB)
+  parms$SB <- new_SB_strain_parameter(n = n_SB, values = values_SB)
   
   # other parameter ---------------------------------------------------------
 
@@ -37,7 +53,7 @@ new_strain_parameter <- function(
   ## oxidisation rate of reduced sulphur
   parms$c = as.numeric(NA)
 
-  if ( length( grep("bush", c(n_CB, n_PB, n_SB)) ) > 0 ) {
+  if ( other == "bush" ) {
     ## substrate diffusivity
     parms$a_S = 0.001
     parms$a_O = 8e-4
@@ -57,22 +73,26 @@ new_strain_parameter <- function(
 }
 
 
-#' Create empty CB strain parameter
+#' Create CB strain parameter
 #'
-#' @param n number of strains or \code{"bush"}, in which case the default from Bush et al 2017 will be used
+#' @param n number of strains 
+#' @param values values to be used or \code{"bush"}, in which case the default from Bush et al (2017) will be used
 #'
 #' @return data.frame with additional class \code{CB_strain_parameter}
 #' @export
 #'
 new_CB_strain_parameter <- function(
-  n = 1
+  n = 1,
+  values = NA
 ){
-  if (n == "bush") {
-    bush <- TRUE
-    n <- 1
-  } else {
-    bush <- FALSE
+  if (is.na(values)) {
+    values <- "NA"
   }
+  
+  if (!(values %in% c("NA", "bush"))) {
+    stop("Not supported value for `valiues`!\n", "Only NA, 'NA' and 'bush' supported!")
+  }
+  
   x <- rep(as.numeric(NA), n)
   nm <- paste0("CB_", 1:n)
   result <- data.frame(
@@ -85,35 +105,39 @@ new_CB_strain_parameter <- function(
     m_CB = x,
     i_CB = x
   )
-  if (bush) {
-    result$g_max_CB = 0.05
-    result$k_CB_P = 0.2
-    result$h_SR_CB = 300
-    result$y_P_CB = 1.67e8
-    result$Pr_CB = 6e-9
-    result$m_CB = 0.02
-    result$i_CB = 0
+  if (values == "bush") {
+    result$g_max_CB = rep(0.05, n)
+    result$k_CB_P = rep(0.2, n)
+    result$h_SR_CB = rep(300, n)
+    result$y_P_CB = rep(1.67e8, n)
+    result$Pr_CB = rep(6e-9, n)
+    result$m_CB = rep(0.02, n)
+    result$i_CB = rep(0, n)
   }
   class(result) <- append( "CB_strain_parameter", class(result))
   return(result)
 }
 
-#' Create empty PB strain parameter
+#' Create PB strain parameter
 #'
-#' @param n number of strains or \code{"bush"}, in which case the default from Bush et al 2017 will be used
-#'
+#' @param n number of strains 
+#' @param values values to be used or \code{"bush"}, in which case the default from Bush et al (2017) will be used
+#' 
 #' @return data.frame with additional class \code{PB_strain_parameter}
 #' @export
 #'
 new_PB_strain_parameter <- function(
-  n = 1
+  n = 1,
+  values = NA
 ){
-  if (n == "bush") {
-    bush <- TRUE
-    n <- 1
-  } else {
-    bush <- FALSE
+  if (is.na(values)) {
+    values <- "NA"
   }
+  
+  if (!(values %in% c("NA", "bush"))) {
+    stop("Not supported value for `valiues`!\n", "Only NA, 'NA' and 'bush' supported!")
+  }
+  
   x <- rep(as.numeric(NA), n)
   nm <- paste0("PB_", 1:n)
   result <- data.frame(
@@ -127,36 +151,40 @@ new_PB_strain_parameter <- function(
     m_PB = x,
     i_PB = x
   )
-  if (bush) {
-    result$g_max_PB = 0.07
-    result$k_PB_SR = 10
-    result$k_PB_P = 0.5
-    result$h_O_PB = 0.5
-    result$y_SR_PB = 1.25e7  ## Y_S_PB in the main text
-    result$y_P_PB = 1.67e8
-    result$m_PB = 0.028
-    result$i_PB = 0
+  if (values == "bush") {
+    result$g_max_PB = rep(0.07, n)
+    result$k_PB_SR = rep(10, n)
+    result$k_PB_P = rep(0.5, n)
+    result$h_O_PB = rep(100, n)
+    result$y_SR_PB = rep(1.25e7, n)  ## Y_S_PB in the main text
+    result$y_P_PB = rep(1.67e8, n)
+    result$m_PB = rep(0.028, n)
+    result$i_PB = rep(0, n)
   }
   class(result) <- append( "PB_strain_parameter", class(result))
   return(result)
 }
 
-#' Create empty SB strain parameter
+#' Create SB strain parameter
 #'
-#' @param n number of strains or \code{"bush"}, in which case the default from Bush et al 2017 will be used
+#' @param n number of strains 
+#' @param values values to be used or \code{"bush"}, in which case the default from Bush et al (2017) will be used
 #'
 #' @return data.frame with additional class \code{SB_strain_parameter}
 #' @export
 #'
 new_SB_strain_parameter <- function(
-  n = 1
+  n = 1,
+  values = NA
 ){
-  if (n == "bush") {
-    bush <- TRUE
-    n <- 1
-  } else {
-    bush <- FALSE
+  if (is.na(values)) {
+    values <- "NA"
   }
+  
+  if (!(values %in% c("NA", "bush"))) {
+    stop("Not supported value for `valiues`!\n", "Only NA, 'NA' and 'bush' supported!")
+  }
+  
   x <- rep(as.numeric(NA), n)
   nm <- paste0("SB_", 1:n)
   result <- data.frame(
@@ -170,15 +198,15 @@ new_SB_strain_parameter <- function(
     m_SB = x,
     i_SB = x
   )
-  if (bush) {
-    result$g_max_SB = 0.1
-    result$k_SB_SO = 5
-    result$k_SB_P = 0.
-    result$h_O_SB = 100
-    result$y_SO_SB = 3.33e7
-    result$y_P_SB = 1.67e8
-    result$m_SB = 0.04
-    result$i_SB = 0
+  if (values == "bush") {
+    result$g_max_SB = rep(0.1, n)
+    result$k_SB_SO = rep(5, n)
+    result$k_SB_P = rep(0.5, n)
+    result$h_O_SB = rep(100, n)
+    result$y_SO_SB = rep(3.33e7, n)
+    result$y_P_SB = rep(1.67e8, n)
+    result$m_SB = rep(0.04, n)
+    result$i_SB = rep(0, n)
   }
   class(result) <- append( "SB_strain_parameter", class(result))
   return(result)
