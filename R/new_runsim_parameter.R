7d4bed17188e2f2c4bacdf7686a1021b272df13f
Rainer M. Krug <Rainer@krugs.de>
2021-11-25 15:20:57 +0100

    Added documentation

--- R/new_runsim_parameter.R
+++ R/new_runsim_parameter.R
@@ -6,7 +6,7 @@
 #'
 #' @return parameter object of the class `runsim_parameter`. 
 #' The object contains the following elements:
-#' - dynamic_model      : the dynamic model to be used. At the moment, only `bushplus_dynamic_model` is implemented. Fir further info, see the documen tation of `bushplus_dynamic_model`.
+#' - dynamic_model      : the dynamic model to be used. At the moment, only `bushplus_dynamic_model` is implemented. Fur further info, see the documen tation of `bushplus_dynamic_model`.
 #' - event_definition   : XXX
 #' - strain_parameter   : object of class `strain_parameter` as returned by `new_strain_parameter()`
 #' - event_interval     : XXX
@@ -25,17 +25,16 @@ new_runsim_parameter <- function(
 ){
   if (help) {
     p <- list(
-      dynamic_model = NA, # default_dynamic_model,
-      event_definition = NA, # default_event_definition,
-      strain_parameter = NA, # default_parameter_values,
-      event_interval = NA, # default_event_interval,
+      dynamic_model = "the dynamic model to be used. At the moment, only `bushplus_dynamic_model` is implemented.",
+      event_definition = "A function which alters the state variables. At the moment only `event_definition_1()` is included. User defined functions with the same signature can be used.",
+      strain_parameter = "object of class `strain_parameter` as returned by `new_strain_parameter()`",
+      event_interval = "interval, in timesteps, in which the event occurs", # default_event_interval,
       noise_sigma = NA, # default_noise_sigma,
-      minimum_abundances = NA, # default_minimum_abundances,
-      sim_duration = NA, # default_sim_duration,
-      sim_sample_interval = NA, # default_sim_sample_interval,
+      minimum_abundances = "Minimum abundances. Smaller abundances will be set to this value duting `event_definition_1()`.", # default_minimum_abundances,
+      sim_duration = "duration of the simulation",
+      sim_sample_interval = "interval, at which the simulation is sampled",
       log10a_series = NA, # default_log10a_series,
-      # initial_state = NA, # default_initial_state,
-      solver_method = "radau" # "radau",
+      solver_method = "Used for the solver. Default is `radau`. For other options, see the documentatioom of `odeSolve::ode`"
     )
   } else {
     p <- list(
@@ -48,7 +47,6 @@ new_runsim_parameter <- function(
       sim_duration = NA, # default_sim_duration,
       sim_sample_interval = NA, # default_sim_sample_interval,
       log10a_series = NA, # default_log10a_series,
-      # initial_state = NA, # default_initial_state,
       solver_method = "radau" # "radau",
     )
     if (!inherits(p, "runsim_parameter")) {
