e6fdc3a0e6ba28ee4949330785f237c94d8b474e
Rainer M. Krug <Rainer@krugs.de>
2022-01-05 14:21:29 +0100

    growth2(): done

--- R/growth2.R
+++ R/growth2.R
@@ -5,8 +5,14 @@
 #' @param g_max Maximum growth rate
 #' @param k_X Half saturation constant for resource X
 #' @param k_Y Half saturation constant for resource Y
+#' 
 #' @return Growth rate
 #' @export
-growth2 <- function(X, Y, g_max, k_X, k_Y) {
+growth2 <- function(
+  X, Y, 
+  g_max, 
+  k_X, 
+  k_Y
+) {
   g_max * ( X / (k_X + X) ) * ( Y / (k_Y + Y))
 }
