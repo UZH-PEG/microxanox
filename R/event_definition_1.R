6012effc36f015ea9f515da8055d0b5731701083
Rainer M. Krug <Rainer@krugs.de>
2022-01-05 13:55:44 +0100

    event_definition_1: done

--- R/event_definition_1.R
+++ R/event_definition_1.R
@@ -1,65 +1,75 @@
-#' A function that contains events that should alter the state variables.
-#' (It is named definition "1", in case other definitions are created.)
-#'
-#' @param times the time point in the simulation
-#' @param y current state variable values
-#' @param parms object of class `strain_parameter`
-#' @param log10a_forcing_func TODO
-#' @param noise_sigma TODO
-#' @param minimum_abundances TODO
+#' Event definition for the simulation
 #' 
-#' @return a vector of the new state variables
+#' This function contains events that do alter the state variables.
+#' It is named definition `_1`, in case other definitions are created.
+#' @param times the time point in the simulation
+#' @param state current state variable values
+#' @param parms object of class `strain_parameter`
+#' @param log10a_forcing_func function to change oxygen diffusivity `a`
+#'   depending on `t`
+#' @param noise_sigma value of variation added to `SO`, `SR`, `O`, and `P` using
+#'   the formula `rnorm(1, 0, noise_sigma * X) * noise_sigma * X`
+#' @param minimum_abundances minimum abundances for CB, PB and SB. if the values
+#'   are lower, they will be set to these `minimum_abundances`
+#' 
+#' @return the state vector, i.e. a vector containing the state variables.
+#' 
 #' @md
 #' @export
 event_definition_1 <- function(
-  times = times, 
-  y = state, 
-  parms = parameters, 
+  times, 
+  state, 
+  parms, 
   log10a_forcing_func, 
   noise_sigma, 
   minimum_abundances
 ) {
   with(
-    as.list(y),
+    as.list(state),
     {
       ## here adding a bit of noise to the abiotics
       SO <- SO + rnorm(1, 0, noise_sigma*SO)*noise_sigma*SO
       SR <- SR + rnorm(1, 0, noise_sigma*SR)*noise_sigma*SR
-      O <- O + rnorm(1, 0, noise_sigma*O)*noise_sigma*O
-      P <- P + rnorm(1, 0, noise_sigma*P)*noise_sigma*P
+      O <-  O  + rnorm(1, 0, noise_sigma*O)*noise_sigma *O
+      P <-  P  + rnorm(1, 0, noise_sigma*P)*noise_sigma *P
       
       ## and below setting the abundance to the minimum, in case it happens to be below it
-      CB <- y[grep("CB", names(y))]
+      CB <- state[grep("CB", names(state))]
       names_CB <- names(CB)[order(names(CB))]
       CB <- as.numeric(CB[order(names(CB))])
       CB[CB < minimum_abundances["CB"]] <- minimum_abundances["CB"]
       
-      PB <- y[grep("PB", names(y))]
+      PB <- state[grep("PB", names(state))]
       names_PB <- names(PB)[order(names(PB))]
       PB <- as.numeric(PB[order(names(PB))])
       PB[PB < minimum_abundances["PB"]] <- minimum_abundances["PB"]
       
-      SB <- y[grep("SB", names(y))]
+      SB <- state[grep("SB", names(state))]
       names_SB <- names(SB)[order(names(SB))]
       SB <- as.numeric(SB[order(names(SB))])
       SB[SB < minimum_abundances["SB"]] <- minimum_abundances["SB"]
       
-      result <- c(CB,
-                  PB,
-                  SB,
-                  SO = SO,
-                  SR = SR,
-                  O = O,
-                  P = P)
+      result <- c(
+        CB,
+        PB,
+        SB,
+        SO = SO,
+        SR = SR,
+        O = O,
+        P = P
+      )
       
-      names(result) <- c(names_CB,
-                         names_PB,
-                         names_SB,
-                         "SO",
-                         "SR",
-                         "O",
-                         "P")
-      result
+      names(result) <- c(
+        names_CB,
+        names_PB,
+        names_SB,
+        "SO",
+        "SR",
+        "O",
+        "P"
+      )
+      
+      return(result)
     }
   )
 }
