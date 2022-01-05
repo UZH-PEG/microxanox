c207d8bd2dcbcefc8fd845a26ca70b1a6d6dc7f0
Rainer M. Krug <Rainer@krugs.de>
2022-01-05 14:52:23 +0100

    new_strain_parameter(): done

--- R/new_strain_parameter.R
+++ R/new_strain_parameter.R
@@ -1,47 +1,51 @@
-#' Creates a set of parameters and starting conditions for a simulation A
-#' function to assist with the initialisation of a simulation, by providing
+#' Returns object of class `strain_parameter`
+#' 
+#' Creates a set of parameters and starting conditions for a simulation. This
+#' function assists with the initialization of a simulation, by providing
 #' various reference sets of parameter values and initial conditions. The
-#' default is to create the parameter set used in Bush et al. (2017) and the
-#' anoxic favourable initial conditions used in the simulations for Figure 2a&b
-#' of that publication.
-#' @param n_CB number of CB strains.
+#' default is to create the parameter set used in Bush et al. (2017)
+#' \url{https://doi.org/10.1093/clinchem/39.5.766} and the anoxic favorable
+#' initial conditions used in the simulations for Figure 2a&b of that
+#' publication.
+#' @param n_CB number of CB strains, default 1.
 #' @param values_CB Allowed values are:
-#' - `"bush"`: default values from Bush et al (2017) will be used for the parameter for CB
+#' - `"bush"`: default values from Bush et al (2017) \url{https://doi.org/10.1093/clinchem/39.5.766} will be used for the parameter for CB
 #' - `"NA"`: all initial values set to `NA`. Usable for e.g. setting own parameter
-#' @param n_PB number of PB strains.
+#' @param n_PB number of PB strains, default 1.
 #' @param values_PB Allowed values are:
-#' - `"bush"`: default values from Bush et al (2017) will be used for the parameter for PB
+#' - `"bush"`: default values from Bush et al (2017) \url{https://doi.org/10.1093/clinchem/39.5.766} will be used for the parameter for PB
 #' - `"NA"`: all parameter will be set to `NA`. Usable for e.g. setting own parameter
-#' @param n_SB number of SB strains.
+#' @param n_SB number of SB strains, default 1.
 #' @param values_SB Allowed values are:
-#' - `"bush"`: default values from Bush et al (2017) will be used for the parameter for SB
+#' - `"bush"`: default values from Bush et al (2017) \url{https://doi.org/10.1093/clinchem/39.5.766} will be used for the parameter for SB
 #' - `"NA"`: all parameter will be set to `NA`. Usable for e.g. setting own parameter
 #' @param values_other  Allowed values are:
-#' - `"bush"`: default values from Bush et al (2017) will be used for additional parameter
+#' - `"bush"`: default values from Bush et al (2017) \url{https://doi.org/10.1093/clinchem/39.5.766}will be used for additional parameter
 #' - `"NA"`: all parameter will be set to `NA`. Usable for e.g. setting own parameter
 #' values to be used for other parameter or \code{"bush"},
 #'   in which case the default from Bush et al (2017) will be used.
 #' @param values_initial_state values to be used for initial values or
 #'   \code{"bush_anoxic"} or \code{"bush_oxic"}, in which case the default from
-#'   Bush et al (2017) will be used.
+#'   Bush et al (2017) \url{https://doi.org/10.1093/clinchem/39.5.766}will be used.
 #' @param help if \code{TRUE}, the parameter will be listed and explained.
+#' 
 #' @return Object of class `strain_parameter`. The object contains the following fields:
 #' The additional parameter are:
 #'  ## Strain parameter
-#'  - CB: strain parameter from Cyano Bacteria
-#'  - PB: strain parameter for the Phototrophic bacteria
-#'  - SB: strain parameter for the Sulphur bacteria
+#'  - `CB`: strain parameter from Cyano Bacteria
+#'  - `PB`: strain parameter for the Phototrophic bacteria
+#'  - `SB`: strain parameter for the Sulphur bacteria
 #'  ## substrate diffusivity
-#'  - a_S: substrate diffusivity of sulphur <- 0.001
-#'  - a_O: substrate diffusivity of oxygen <- 8e-4
-#'  - a_P: substrate diffusivity of phosphorous <- 0.01
+#'  - `a_S`: substrate diffusivity of sulphur <- 0.001
+#'  - `a_O`: substrate diffusivity of oxygen <- 8e-4
+#'  - `a_P`: substrate diffusivity of phosphorous <- 0.01
 #'  ## background substrate concentration
-#'  - back_SR: background substrate concentration of XXX <- 300
-#'  - back_SO: background substrate concentration of XXX <- 300
-#'  - back_O: background substrate concentration of oxygen <- 300
-#'  - back_P: background substrate concentration of phosphorous <- 9.5
+#'  - `back_SR`: background substrate concentration of XXX <- 300
+#'  - `back_SO`: background substrate concentration of XXX <- 300
+#'  - `back_O`: background substrate concentration of oxygen <- 300
+#'  - `back_P`: background substrate concentration of phosphorous <- 9.5
 #'  ## oxidisation rate of reduced sulphur
-#'  - c: <- 4e-5
+#'  - `c`: <- 4e-5
 #' @md
 #' @export
 #'
@@ -102,25 +106,16 @@ new_strain_parameter <- function(
     parms$c <- 4e-5
   }
   
-  parms$initial_state <- new_initial_state(n_CB = n_CB, n_PB = n_PB, n_SB = n_SB, values = values_initial_state)
+  parms$initial_state <- new_initial_state(
+    n_CB = n_CB, 
+    n_PB = n_PB, 
+    n_SB = n_SB, 
+    values = values_initial_state
+  )
   
   parms$ss_expt <- NULL
   
   class(parms) <- append("strain_parameter", class(parms))
   
-  if (help) {
-    # parms$CB <- new_CB_strain_parameter(help = TRUE)
-    # parms$PB <- new_PB_strain_parameter(help = TRUE)
-    # parms$SB <- new_CB_strain_parameter(help = TRUE)
-    parms$a_S<- "substrate diffusivity of sulphur"
-    parms$a_O <-  "substrate diffusivity of oxygen"
-    parms$a_P<- "substrate diffusivity of phosphorous"
-    parms$back_SR <- "background substrate concentration of TODO"
-    parms$back_SO <- "background substrate concentration of TODO"
-    parms$back_O <- "background substrate concentration of oxygen"
-    parms$back_P <- "background substrate concentration of phosphorous"
-    parms$c <- "oxidisation rate of reduced sulphur"
-    # parms$initial_state <- new_initial_state(help = TRUE)
-  }
   return(parms)
 }
