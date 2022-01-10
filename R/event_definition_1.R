18b2ad5e537dc21c17d0f80008cf406d87f42aeb
Rainer M. Krug <Rainer@krugs.de>
2022-01-10 16:38:46 +0100

    Vignettes Checked = working now

--- R/event_definition_1.R
+++ R/event_definition_1.R
@@ -23,33 +23,33 @@ event_definition_1 <- function(
   log10a_forcing_func,
   noise_sigma,
   minimum_abundances
-) {
+){
   with(
     as.list(state),
     {
       ## here adding a bit of noise to the abiotics
-      if(noise_sigma!=0) {
+      # if(noise_sigma!=0) {
       SO <- SO + rnorm(1, 0, noise_sigma*SO)*noise_sigma*SO
       SR <- SR + rnorm(1, 0, noise_sigma*SR)*noise_sigma*SR
       O <-  O  + rnorm(1, 0, noise_sigma*O)*noise_sigma *O
       P <-  P  + rnorm(1, 0, noise_sigma*P)*noise_sigma *P
-
+      
       ## and below setting the abundance to the minimum, in case it happens to be below it
       CB <- state[grep("CB", names(state))]
       names_CB <- names(CB)[order(names(CB))]
       CB <- as.numeric(CB[order(names(CB))])
       CB[CB < minimum_abundances["CB"]] <- minimum_abundances["CB"]
-
+      
       PB <- state[grep("PB", names(state))]
       names_PB <- names(PB)[order(names(PB))]
       PB <- as.numeric(PB[order(names(PB))])
       PB[PB < minimum_abundances["PB"]] <- minimum_abundances["PB"]
-
+      
       SB <- state[grep("SB", names(state))]
       names_SB <- names(SB)[order(names(SB))]
       SB <- as.numeric(SB[order(names(SB))])
       SB[SB < minimum_abundances["SB"]] <- minimum_abundances["SB"]
-
+      
       result <- c(
         CB,
         PB,
@@ -59,7 +59,7 @@ event_definition_1 <- function(
         O = O,
         P = P
       )
-
+      
       names(result) <- c(
         names_CB,
         names_PB,
@@ -69,7 +69,7 @@ event_definition_1 <- function(
         "O",
         "P"
       )
-
+      
       return(result)
     }
   )
