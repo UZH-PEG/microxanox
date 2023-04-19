42d3eaf286505a5174ec1567c6ff8f650a63478b
pascalbartschi <103056293+pascalbartschi@users.noreply.github.com>
2023-04-19 19:27:22 +0200

    plotting function

---
+++ R/plot_symmetry_measures.R
@@ -0,0 +1,63 @@
+#' Visualizes the symmetry measures obtained by the function \code{get_symmetry_measures()}
+#' 
+#' @param res Results from \code{run_temporal_ssfind()} or from \code{run_temporal_ssfind_symmetric()}
+#' @param species Environmental varibale of which the symmetry measures shoudl be plotted
+#' @return ggplot object, with shifts visualized as arrows and hysteresis area ribbon
+#' 
+#' @global
+#' 
+#' @importFrom dplyr rename mutate select filter
+#' @importFrom ggplot2 geom_path geom_point geom_segment geom_ribbon 
+#' 
+
+
+plot_symmetry_measures <- function(species, 
+                                   res){
+  
+  measures <- get_symmetry_measures(res)
+  ## check whether bush or symmetry simulation prepare dataframe scale_fill_manual scale_color_manual labs
+  if ("a_O" %in% colnames(res)){
+    res <- res %>% 
+      rename(aO = a_O) %>%
+      mutate(recovery = ifelse(direction == "up", "oxic", "anoxic")) %>%
+      select(-direction)
+  } else {
+    res <- res %>%
+      select(-starts_with("PB"), -aS, -SO )
+  }
+  # speciesmeasures <- gather(measures[species,], key = "measure", value = "value")
+  ## data wrangling to plotting-mature frame with xy coords in seperate columns
+  
+  plot_df <- data.frame(aO = as.numeric(select(measures[species,], c(1, 2, 5, 6))),
+                        state = as.numeric(select(measures[species,], c(3, 4, 7, 8))),
+                        measure = as.character(colnames(measures)[c(1, 2, 5, 6)])) 
+  ribbon_temp <- res %>% filter((aO >= median(measures$ox_TP)), (aO <= median(measures$anox_TP))) %>% select(c(species, recovery, aO))
+  ribbon_df <- data.frame(oxic = rev(ribbon_temp %>% filter(recovery == "oxic") %>% pull(species)), 
+                          anoxic = ribbon_temp %>% filter(recovery == "anoxic") %>% pull(species), 
+                          aO = ribbon_temp %>% filter(recovery == "anoxic") %>% pull(aO))
+  
+  p <- ggplot() +
+    # plot the data
+    geom_path(mapping = aes(x = res$aO, , y = res[,species], linetype = res$recovery)) +
+    # plot the tipping points
+    geom_point(data = slice(plot_df, c(1, 3)), mapping = aes(x = aO, y = state, color = measure)) +
+    # plot extent of shifts
+    geom_segment(data = slice(plot_df, c(1, 2)),
+                 mapping = aes(x = aO[1], y = state[1], xend = aO[2], yend = state[2], group = "recovery"),
+                 color = "red",
+                 lineend = "round", linejoin = "round", size = 0.5,  
+                 arrow = arrow(length = unit(0.5, "cm"))) +
+    geom_segment(data = slice(plot_df, c(3, 4)),
+                 mapping = aes(x = aO[1], y = state[1], xend = aO[2], yend = state[2], group = "collapse"),
+                 color = "green",
+                 lineend = "round", linejoin = "round", size = 0.5,  
+                 arrow = arrow(length = unit(0.5, "cm"))) + 
+    geom_ribbon(data = ribbon_df,
+                mapping = aes(x = aO, ymin = oxic, ymax = anoxic, fill = "hysteresis area"), alpha = 0.3) +
+    scale_fill_manual(values = c("blue")) +
+    scale_color_manual(values = c("red", "green")) + 
+    labs(x = "aO", y = species, fill = "measures", color = "measures", linetype = "recovery") +
+    theme_bw()
+  
+  return(p)
+}
